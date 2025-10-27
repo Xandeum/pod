79bcbce40252ee80e76d6999f48ab3a9e8057374
Maverick9081 <faliyaabhishek@gmail.com>
2025-10-27 12:01:47 +0530

    updated idle timeout to 1 min instead of 30 mins

--- src/client.rs
+++ src/client.rs
@@ -944,7 +944,7 @@ pub fn configure_client() -> Result<ClientConfig> {
 
     let mut transport_config = TransportConfig::default();
     transport_config
-        .max_idle_timeout(Some(Duration::from_secs(24 * 60 * 60).try_into().map_err(|e| anyhow!("Invalid timeout: {}", e))?))
+        .max_idle_timeout(Some(Duration::from_secs(60).try_into().map_err(|e| anyhow!("Invalid timeout: {}", e))?))
         .stream_receive_window(VarInt::from_u64(1_000_000).map_err(|e| anyhow!("Invalid stream window: {}", e))?) 
         .max_concurrent_bidi_streams(VarInt::from_u64(100).map_err(|e| anyhow!("Invalid stream count: {}", e))?);
 
